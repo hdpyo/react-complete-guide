c9d2fe86e746d14e50feaa138dbcadbaa4c1d05a
hdpyo <vvhooping@gmail.com>
2022-07-21 22:56:50 +0900

    feat: useState 활용하기

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
+import { useState } from 'react';
+
 import './ExpenseItem.css';
 
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 export default function ExpenseItem({ title, date, amount }) {
+  const [newTitle, setNewTitle] = useState(title);
+
   const clickHandler = () => {
-    console.log('clicked!');
+    setNewTitle('Updated!');
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className="expense-item__price">${amount}</div>
         <button type="button" onClick={clickHandler}>
           Change Title
