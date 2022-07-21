1935798bcc8cca0e286eb10572c40b91591dcb9b
hdpyo <vvhooping@gmail.com>
2022-07-21 21:31:08 +0900

    feat: 세션 3. 리액트 기초 및 실습 컴포넌트

---
+++ 01-starting-setup/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/no-unescaped-entities': 'off',
+    ' react/jsx-one-expression-per-line': 'off',
+  },
+};
